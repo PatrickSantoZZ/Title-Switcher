9004d701683024f85783e491bf08d5dcb3bd1a7c
Nekonekoo <patrickfetscher60@gmail.com>
2019-05-09 21:21:46 +0200

    rewritten Version

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
-# Title-Switcher
-A tera-proxy module that changes your title server side.<br/><br/>
+# Title-Changer
+A tera-proxy module that changes your title CLIENT side.<br/><br/>
 
 ## Note
 * __Requires command module.__<br/>
-* __This is highly detectable and it will get you banned if you're caught, use at your own risk.__
+* __Its just a Client-side module.__
 
 ## Usage
 
-1. `/proxy title <insert id here>`
-2. You can get the Title ID from [HERE](http://teradatabase.net/us/achievements/reward/rtitles/).
+__Use this Commands in your Proxy Tab__
+* /proxy title set <insert id here>` 
+*You can get the Title ID from [HERE](https://teralore.com/en/achievements/reward/rtitles/).
+*`/proxy title debug - Debug Mode select an title to obtain the ID.
 
 ## Credits
-* Original Author: Eeemj
-* Updated for new Patch: Nekonekoo
+* Exploit Idea by: Eeemj
--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-const command = require('command');
-const pars = require('tera-data-parser');
-const command = command(mod);
+module.exports = function Client_Side_Title(mod) {
 
+    let title_id = null;
+    let debug = false;
 
-module.exports = function Title(mod) {
+    mod.command.add('title', (arg_1, arg_2) => {
+        if (arg_1 === 'set') {
+            if (arg_2 !== undefined && !isNaN(arg_2)) {
+                title_id = Number.parseInt(arg_2);
+                mod.send('S_APPLY_TITLE', 3, {
+                    gameId: mod.game.me.gameId,
+                    title: title_id
+                });
+                mod.command.message(`Successfully set your title id to ${title_id}.`);
+            }
+        }
+        else if (arg_1 === 'debug') {
+            debug = !debug;
+            mod.command.message('Debug mode activated please select an title to obtain the id.');
+        }
+    });
 
-	mod.hook("S_APPLY_TITLE", 3 , (event) =>  {
-	
-		));
-
-		command.add('title', (id) => {
-		if (id) mod.toServer('S_APPLY_TITLE', 3, { id });
-	});
-	]));
-
-
+    mod.hook('S_APPLY_TITLE', 3, (event) => {
+        if (debug) {
+            mod.log(`Your currently selected title id is ${event.title}.`);
+        }
+    });
 };
